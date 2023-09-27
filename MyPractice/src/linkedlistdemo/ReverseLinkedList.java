4386552771925f098221f50529cdda186712ab0d
Archana <archooworship@gmail.com>
2023-09-26 23:05:45 -0700

    node class added

--- MyPractice/src/linkedlistdemo/ReverseLinkedList.java
+++ MyPractice/src/linkedlistdemo/ReverseLinkedList.java
@@ -1,8 +1,23 @@
 package linkedlistdemo;
 // we have given the linkedList that need to reverse.
 public class ReverseLinkedList {
+	Node head;
+	
+	class Node{
+		String  data;
+		Node next;
+		
+		public Node( String data) {
+			this.data = data;
+			this.next = null;
+			
+		}
+		
+	}
 
 	public static void main(String[] args) {
+		
+		
 		
 
 	}
