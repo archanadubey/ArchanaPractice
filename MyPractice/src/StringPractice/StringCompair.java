3bb2df2293257c0d2a640c24033f63620705f23a
Archana <archooworship@gmail.com>
2024-01-07 01:23:47 -0800

    String compair commited

---
+++ MyPractice/src/StringPractice/StringCompair.java
@@ -0,0 +1,33 @@
+package StringPractice;
+
+public class StringCompair {
+
+	public static void main(String[] args) {
+		
+		String s = "I love myself";
+		String s1 = "I  myself";
+		String s2 = "Love YourSelf";
+		// "==" is used for reference equality, 
+		//which means that it checks whether two objects refer to the same memory location. 
+		System.out.println(s==s1);
+		System.out.println(s.equals(s1));
+		
+		System.out.println(s2==s1);
+		
+		
+		
+		String hs = new String("name");
+		String scp = hs.toString();
+		
+		System.out.println(scp);
+		
+		// concat
+		String str = "Arch";
+		str.concat("ana");
+		
+		System.out.println(str);
+		
+
+	}
+
+}
